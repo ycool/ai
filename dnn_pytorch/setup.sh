7b3d657275a7eab9c1fb499b40976e8f22568a5c
Jiangtao Hu <hujiangtao@baidu.com>
2020-02-14 11:25:36 -0800

    pytorch: add dog cat cnn demo gpu version.

--- dnn_pytorch/setup.sh
+++ dnn_pytorch/setup.sh
@@ -12,5 +12,12 @@ pip install torchvision
 
 pip install numpy tqdm
 
+pip install jupyterlab
+
+# wget https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip
+
+# jupyter lab --allow-root --ip=0.0.0.0
+
+
 
 
